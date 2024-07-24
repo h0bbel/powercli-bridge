215a3ed8177975d7c7b6cb35e1694975c92461a3
Christian Mohn <christian@drible.net>
2024-07-24 12:00:15 +0200

    Update ups.ps1

--- endpoints/ups.ps1
+++ endpoints/ups.ps1
@@ -1,11 +1,11 @@
-$endpoint = "UPS"
+$endpoint = "UPS Shutdown"
 $version = "0.1"
 Write-PodeJsonResponse -Value @{ "success" = "true";"version"= "$endpoint version:$version";"message"= "NUT/UPS initiated shutdown started."}
-Write-Host "UPS triggered - Running"
+Write-Host "$endpoint version:$version UPS Shutdown event triggered - Running"
 
 # Standard Definitions
 $UPSdate = Get-Date -Format "dd/MM/yyyy HH:mm K"
-$VMDescription = "$UPSdate : UPS event detected, shutting down"  
+$VMDescription = "$UPSdate : UPS shutdown event detected, shutting down"  
 
 # Define vCenter Server details
 # Could this be included from external source?
@@ -20,7 +20,7 @@ Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out
 # Connect to vCenter Server
 try {
     Connect-VIServer -Server $vCenterServer -User $Username -Password $Password -ErrorAction Stop
-    Write-Host "Connected to vCenter Server successfully."
+    Write-Host "Connected to vCenter Server $vCenterServer successfully."
 }
 catch {
     Write-Host "Failed to connect to vCenter Server: $($_.Exception.Message)" -ForegroundColor Red
@@ -42,11 +42,20 @@ catch {
 
 ### Actual logic
 ### Get all powered on VMs (except vCLS) and shut them down!
+### What about vCenter? How to ensure it is shut down last?
+###     Use tags for this? Exclude a tag until no VMs are powered on, and then power off the vc vm?
 #$VMs = Get-VM | Where-Object {$_.powerstate -eq ‘PoweredOn’} | Where-Object -Property Name -NotLike "vCLS*" | Shutdown-VMGuest -Confirm:$false
 
-# NOTE: This does POWER OFF, not graceful SHUTDOWN.
+# NOTE: This does POWER OFF, not graceful SHUTDOWN
+# https://github.com/voletri/PowerCLI-1/blob/master/Power-Off-VMs.ps1 has logic for checking VMware Tools status in VM (line 99)
+#       This also overwrites the notes field it does not retain existing notes if any.
+#       Would it be better to create a logfile instead?
+
 $VMs = Get-VM | Where-Object {$_.powerstate -eq ‘PoweredOn’} | Where-Object -Property Name -NotLike "vCLS*" | Set-VM -Description $VMDescription -confirm:$false | Stop-VM -Confirm:$false
 Write-Host "Discovered these powered on VMs: $VMs - Shutting down"
+
+
+
 
 # Add logic that waits x amount of time after graceful shutdown, rescans and does power off?
 # Shutdown-VMGuest/Stop-VM doesn't work without VMware tools
@@ -59,4 +68,4 @@ Write-Host "Discovered these powered on VMs: $VMs - Shutting down"
 # Completed
 Write-Host "Disconnecting from $vCenterServer"
 Disconnect-VIServer -Server $vCenterServer -Force -Confirm:$false
-Write-Host "All defined tasks have run, task completed."
+Write-Host "All defined UPS shutdown tasks have run, task completed."
