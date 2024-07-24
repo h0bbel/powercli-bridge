3e808eed614e548fc0561e489d804b0e0360112e
Christian Mohn <christian@drible.net>
2024-07-24 11:03:31 +0200

    Added logic for poweroff ALL VMs except vCLS.

--- endpoints/ups.ps1
+++ endpoints/ups.ps1
@@ -28,24 +28,35 @@ catch {
     exit
 }
 
+# Proof of Concept Logic
 # Temporary test VM
 # Currently sets VM description to a timestamp, and forcefully shuts it down.
 # Needs logic to loop through all VMs and shut them down gracefully.
 
-$TestVM = "ups-dummy-vm01"
-Write-Host "Abusing " $TestVM
-Write-Host "Setting VM description to" $VMDescription "on" $TestVM
-Set-VM $TestVM -Description $VMDescription -confirm:$false
-# Get-VM $TestVM | Shutdown-VMGuest -Confirm:$false # Shut down requires VMware tools
-Get-VM $TestVM | Stop-VM -Confirm:$false
+#$TestVM = "ups-dummy-vm01"
+#Write-Host "Abusing $TestVM Setting VM description to $VMDescription"
+#Set-VM $TestVM -Description $VMDescription -confirm:$false
+#Get-VM $TestVM | Shutdown-VMGuest -Confirm:$false # Shut down requires VMware tools
+#Get-VM $TestVM | Stop-VM -Confirm:$false
 
+
+### Actual logic
+### Get all powered on VMs (except vCLS) and shut them down!
+#$VMs = Get-VM | Where-Object {$_.powerstate -eq ‘PoweredOn’} | Where-Object -Property Name -NotLike "vCLS*" | Shutdown-VMGuest -Confirm:$false
+
+# NOTE: This does POWER OFF, not graceful SHUTDOWN.
+$VMs = Get-VM | Where-Object {$_.powerstate -eq ‘PoweredOn’} | Where-Object -Property Name -NotLike "vCLS*" | Set-VM -Description $VMDescription -confirm:$false | Stop-VM -Confirm:$false
+Write-Host "Discovered these powered on VMs: $VMs - Shutting down"
+
+# Add logic that waits x amount of time after graceful shutdown, rescans and does power off?
 # Shutdown-VMGuest/Stop-VM doesn't work without VMware tools
-# Add in power-off for VMs that are still running after x amount of time?
+
 
 # Get all hosts
 #$ESXiHost = Get-VMHost
 #Write-Host "Found ESXi Hosts:" $ESXiHost
 
 # Completed
+Write-Host "Disconnecting from $vCenterServer"
 Disconnect-VIServer -Server $vCenterServer -Force -Confirm:$false
-Write-Host "All defined tasks have run"
+Write-Host "All defined tasks have run, task completed."
