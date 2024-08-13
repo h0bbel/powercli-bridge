fb64a98c6deb770b6be8c255446a4b496d509b47
Christian Mohn <christian@drible.net>
2024-08-13 11:47:15 +0200

    Add comment-based help

--- wrapper.ps1
+++ wrapper.ps1
@@ -1,5 +1,18 @@
 # Wrapper script to environment variables are set before starting the Pode Server
 
+#https://learn.microsoft.com/en-us/powershell/scripting/developer/help/syntax-of-comment-based-help?view=powershell-7.4
+
+<#
+.SYNOPSIS
+  Ensure environment variables are set before starting Pode server
+.DESCRIPTION
+  Runs setenv.ps1 to ensure environment variables are set before starting Pode server.
+.LINK 
+  https://github.com/h0bbel/powercli-bridge/
+.EXAMPLE
+  PS> .\wrapper.ps1
+#>
+
 $SetEnv = "$PSScriptRoot\setenv.ps1"
 $PodeServer = "$PSScriptRoot\server.ps1"
 
